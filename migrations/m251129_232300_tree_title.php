0daf90234b4bfcabf0e112ab5b3e589ccc118701
Ricardo Crescenti <ricardo.crescenti@gmail.com>
2025-12-07 17:06:31 -0300

    Retirado linha da migração que preence o `tree_title` com o `title`, porque agora se o `tree_title` for nulo vai usar o `title` automaticamente.

--- migrations/m251129_232300_tree_title.php
+++ migrations/m251129_232300_tree_title.php
@@ -13,9 +13,6 @@ class m251129_232300_tree_title extends Migration
     public function safeUp()
     {
         $this->addColumn('wiki_page', 'tree_title', $this->string(255));
-        $this->update('wiki_page', [
-            'tree_title' => new \yii\db\Expression('title'),
-        ], 'tree_title IS NULL');
     }
 
     /**
